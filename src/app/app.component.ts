a54f13e6b5781e808f8144d45a1b32db1a25abdc
Kumar Kasimala <kkasimala@salesforce.com>
2018-09-27 11:48:12 -0700

    Initial changes to shopping cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductList } from './mockData/productlist';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-cart';
+  products = [];
+  cartlist = [];
+  isShowCart = false;
+  constructor(){
+    this.products = new ProductList().getData();
+  }
+
+  onSelect(product){
+    this.cartlist.push(product);
+  }
+
+  showCart(){
+    this.isShowCart = true;
+  }
+
+  showProducts(){
+    this.isShowCart = false;
+  }
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { ProductComponent } from './product/product.component';
+import { CartListComponent } from './cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartItemComponent,
+    ProductComponent,
+    CartListComponent
   ],
   imports: [
     BrowserModule
